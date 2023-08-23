b418a924c8468020a8f68a28bd9679891327daa9
eComS <eComS@live.com>
2023-08-23 22:57:01 +0700

    123

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -7,9 +7,9 @@ def index(request):
     context = {
         'advertisements': advertisements
     }
-    return render(request, 'index.html', context)
+    return render(request, 'app_advertisements/index.html', context)
 def top_sellers(request):
-    return render(request, 'top-sellers.html')
+    return render(request, 'app_advertisements/top-sellers.html')
 
 
 def advertisement_post(request):
@@ -24,5 +24,5 @@ def advertisement_post(request):
     else:
         form = AdvertisementForm()
     context = {'form': form}
-    return render(request, 'advertisement-post.html', context)
+    return render(request, 'app_advertisements/advertisement-post.html', context)
 
--- advertisements/app_auth/urls.py
+++ advertisements/app_auth/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
+from django.urls import path
+from .views import profile_view,login_view,logout_view
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include("app_advertisements.urls")),
-    path('myauth/',include("app_auth.urls")),
+    path("profile/", profile_view, name="profile"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
