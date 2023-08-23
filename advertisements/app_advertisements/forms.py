7f1d4489512279f5276b146cae91df6a84675a73
eComS <eComS@live.com>
2023-08-23 21:52:14 +0700

    123

--- advertisements/app_advertisements/forms.py
+++ advertisements/app_advertisements/forms.py
@@ -1,12 +1,22 @@
 from django import forms
+from .models import Advertisements
+from django.core.exceptions import ValidationError
 
+class AdvertisementForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['description'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['price'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['image'].widget.attrs['class'] = 'form-control form-control-lg'
+        self.fields['auction'].widget.attrs['class'] = 'form-check-input'
 
-class AdvertisementForm(forms.Form):
-    title = forms.CharField(max_length=64, widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}))
-    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control form-control-lg'}))
-    price = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control form-control-lg'}))
+    class Meta:
+        model = Advertisements
+        fields = ['title', 'description', 'price', 'auction', 'image']
 
-    auction = forms.BooleanField(widget=forms.CheckboxInput(
-        attrs={'class': 'form-check-input'}), required=False
-    )
-    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control form-control-lg'}))
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if title.startswith("?"):
+            raise ValidationError('Заголовок не может начинаться с "?"')
+        return title
